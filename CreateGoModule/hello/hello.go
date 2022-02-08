d1a41b4663f45ac0bd8ff8ab9ac19abe0361791c
Tiago Barbosa <tiago@farias.cc>
2022-02-07 22:10:34 -0300

    Initial commit - Golang studies

---
+++ CreateGoModule/hello/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"example.com/greetings"
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.SetPrefix("greetings:")
+	log.SetFlags(0)
+	
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	fmt.Println(message)
+}
